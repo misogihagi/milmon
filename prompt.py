031397d893d7d83cde0ee2c6b6a3285f4fdeb681
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-08-19 02:37:33 +0900

    Create prompt.py

---
+++ prompt.py
@@ -0,0 +1,44 @@
+やせ　普通　太い
+１頭身　２頭身 ３頭身　４頭身
+ロングライフ　きりっと　凛々しく　力強い　柔らかく親しみやすい
+ホモジナイズ　リアル　デフォルメ
+
+ツンデレ
+気分屋
+賢い
+いたずらっ子 
+活発
+感情豊か
+甘えん坊
+寂しがり
+優しい
+穏やか
+大人しい
+のんびり 
+社交的 
+
+
+性格　内容量　乳脂肪分　（保存方法）　（ホモジナイズ）
+
+prompt = "{homogenized}, centrally positioned, is depicted against a plain white background. The character is white, {tall}, round creature. Its eyes are large {longlife}. Its mouth is a cheerful smile.  The character's has the friendly expression. The character is {body} and in a pose of {character}. The perspective is straight-on, from a slightly elevated viewpoint. The lighting is even, creating a bright and cheerful atmosphere. The overall style is cartoonish and playful.  The character appears childlike, lacking any detailed features beyond its friendly expression and playful pose. There are no accessories, and no discernible clothing.  The composition is simple and clean, focusing on the {character} expression and action of the character."
+
+characters = [
+"Tsundere",
+"Moody",
+"Smart",
+"Mischievous",
+"Active",
+"Emotional",
+"Spoiled",
+"Lonely",
+"Kind",
+"Calm",
+"Quiet",
+"Relaxed",
+"Sociable",
+]
+
+body = "thin" "normal" "fat"
+tall = "one-head-tall" "two-head-tall" "three-head-tall" "four-head-tall"
+homogenized = "A cartoon character" "A 3d character"
+longlife = "" "and Dignified"
